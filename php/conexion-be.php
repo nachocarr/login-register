1afe06390857e86c88491d51a0d5ec54b9156e04
nachocarr <nacarrillo.lo@gmail.com>
2026-08-23 18:11:54 +0200

    Probar conexion directa

--- php/conexion-be.php
+++ php/conexion-be.php
@@ -1,5 +1,8 @@
-<?php 
-
+<?php
+// Forzar visualización de errores de PHP en pantalla
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $host = getenv('host');
 $user = getenv('user');
@@ -7,14 +10,18 @@ $password = getenv('password');
 $dbname = getenv('dbname');
 $port = getenv('PORT');
 
-$conexion = mysqli_connect($host, $user, $password, $dbname, $port);
-    /*
-    if($conexion){
-        echo "conexion establecida";
-    }else{
-
-    echo " no se ha conectado";
+// Intentar conectar y atrapar cualquier fallo de mysqli
+try {
+    $conexion = mysqli_connect($host, $user, $password, $dbname, (int)$port);
+    
+    if (!$conexion) {
+        throw new Exception(mysqli_connect_error());
     }
-    */
-
+    // Si llega aquí, conectó con éxito
+    echo "¡Conexión exitosa a la base de datos!";
+} catch (Exception $e) {
+    echo "<h3 style='color:red;'>Fallo crítico de conexión MySQL:</h3>";
+    echo "<pre>" . $e->getMessage() . "</pre>";
+    exit();
+}
 ?>
