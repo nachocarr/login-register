1cd8572483b799f46fc0302eab4ef923bbec361d
nachocarr <nacarrillo.lo@gmail.com>
2026-08-23 18:22:04 +0200

    Añadir conexion segura con SSL para Aiven

--- php/conexion-be.php
+++ php/conexion-be.php
@@ -1,27 +1,26 @@
 <?php
-// Forzar visualización de errores de PHP en pantalla
+// Forzar visualización de errores
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $host = getenv('host');
 $user = getenv('user');
 $password = getenv('password');
 $dbname = getenv('dbname');
-$port = getenv('PORT');
+$port = (int)getenv('PORT');
 
-// Intentar conectar y atrapar cualquier fallo de mysqli
-try {
-    $conexion = mysqli_connect($host, $user, $password, $dbname, (int)$port);
-    
-    if (!$conexion) {
-        throw new Exception(mysqli_connect_error());
-    }
-    // Si llega aquí, conectó con éxito
-    echo "¡Conexión exitosa a la base de datos!";
-} catch (Exception $e) {
-    echo "<h3 style='color:red;'>Fallo crítico de conexión MySQL:</h3>";
-    echo "<pre>" . $e->getMessage() . "</pre>";
-    exit();
+// Inicializar MySQLi con soporte SSL para Aiven
+$conexion = mysqli_init();
+
+// Configurar opciones de SSL para evitar bloqueos por certificados en la nube
+mysqli_ssl_set($conexion, NULL, NULL, NULL, NULL, NULL);
+mysqli_options($conexion, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
+
+// Intentar la conexión real utilizando el puerto y SSL
+$conex_exitosas = mysqli_real_connect($conexion, $host, $user, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL);
+
+if (!$conex_exitosas) {
+    // Si falla, mostramos el error exacto en pantalla en lugar del Error 500
+    die("<h3 style='color:red;'>Error de conexión SSL con Aiven:</h3> " . mysqli_connect_error());
 }
 ?>
